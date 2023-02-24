ca0cebbe1e083f8f2314a55206541d32355b057b
T-erry <terrywachuka10@gmail.com>
2023-02-24 14:18:51 +0300

    completed assignment

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,11 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    #A character will belong to a show (the show migration is already provided) and an actor
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    end
   end
 end
